d1bbfa27f348461b508b03a8f45a8d2b3a0b7394
huangyuanyin <3443718488@qq.com>
2024-08-12 17:42:26 +0800

    04_multi_selector

--- src/components/CatController.tsx
+++ src/components/CatController.tsx
@@ -1,10 +1,25 @@
+import shallow from 'zustand/shallow'
 import { useCatStore } from '../stores/catStore'
 
 export const CatController = () => {
-  //   const { increaseBigCats, increaseSmallCats } = useCatStore(); // 会导致组件重新渲染
+  // 第一种写法：会导致组件重新渲染
+  //   const { increaseBigCats, increaseSmallCats } = useCatStore();
 
-  const increaseBigCats = useCatStore.use.increaseBigCats()
-  const increaseSmallCats = useCatStore.use.increaseSmallCats()
+  // 第二种写法： 利用 selector，不会导致组件重新渲染
+  // const increaseBigCats = useCatStore.use.increaseBigCats()
+  // const increaseSmallCats = useCatStore.use.increaseSmallCats()
+
+  // 第三种写法：在 store 中一次选择多个状态【返回的是object】
+  // const { increaseBigCats, increaseSmallCats } = useCatStore(
+  //   (state) => ({
+  //     increaseBigCats: state.increaseBigCats,
+  //     increaseSmallCats: state.increaseSmallCats
+  //   }),
+  //   shallow // 判断第一层状态是否相等
+  // )
+
+  // 第四种写法：在 store 中一次选择多个状态【返回的是array】
+  const [increaseBigCats, increaseSmallCats] = useCatStore((state) => [state.increaseBigCats, state.increaseSmallCats], shallow)
 
   return (
     <div className='box'>
